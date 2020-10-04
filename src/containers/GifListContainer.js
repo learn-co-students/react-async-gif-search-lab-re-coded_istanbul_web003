6e86de7cde40ea66694e0d5aa6b537a12fad3ecd
ihab-sensei <ib-mashhadani@hotmail.com>
2020-10-04 11:18:10 +0300

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -3,16 +3,12 @@ import GifList from "../components/GifList";
 import GifSearch from "../components/GifSearch";
 class GifListContainer extends Component {
   state = {
-    query: "cats",
     data: [],
   };
 
   handleSearch(keyWord) {
-    this.setState({ query: keyWord });
-  }
-  componentDidMount() {
     fetch(
-      `https://api.giphy.com/v1/gifs/search?q=${this.state.query}&api_key=dc6zaTOxFJmzC&rating=g`
+      `https://api.giphy.com/v1/gifs/search?q=${keyWord}&api_key=dc6zaTOxFJmzC&rating=g`
     )
       .then((res) => res.json())
       .then((data) => this.setState({ data: data.data.slice(0, 3) }));

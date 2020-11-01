7c1858c9581a9bc71bd282919c94f2a53cd28869
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-11-02 01:04:29 +0300

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -5,12 +5,8 @@ import GifList from '../components/GifList';
 //should be a container that does data fetching and 
 //then renders its corresponding sub-component.
 const GIPHY_API_KEY = "D1RgdpOVewekctGA9Lr9WfUMDq1Zaggx";
-const URL =
-  "https://api.api.giphy.com/v1/gifs/search?q=${}&rating=g" +
-  `api-key=${GIPHY_API_KEY}`;
-
-https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=YOUR API KEY&rating=g
-
+const URL = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${QUERY}&limit=25&offset=0&rating=g&lang=en`;
+const QUERY = "rickandmorty";
 
 class GifListContainer extends Component {
   state={
@@ -18,10 +14,12 @@ class GifListContainer extends Component {
   }
 
   fetchGifs=(search)=>{
-    fetch('https://api.giphy.com/v1/gifs/search?q=${search}&api_key=D1RgdpOVewekctGA9Lr9WfUMDq1Zaggx=g')
+    fetch(URL)
     .then(response=> response.json())
     .then(({data})=>{
-      this.setState({gifs:data.map(gif=>({url:gif.images.original.url}))})
+      this.setState({
+        gifs: data.map((gif) => ({ url: gif.images.fixed_height.url })),
+      });
     })
   }
 

7eb4b07a4ea7c698a2d14897921e89fcbd16ebb1
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-11-02 01:06:47 +0300

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -5,8 +5,9 @@ import GifList from '../components/GifList';
 //should be a container that does data fetching and 
 //then renders its corresponding sub-component.
 const GIPHY_API_KEY = "D1RgdpOVewekctGA9Lr9WfUMDq1Zaggx";
-const URL = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${QUERY}&limit=25&offset=0&rating=g&lang=en`;
 const QUERY = "rickandmorty";
+const URL = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${QUERY}&limit=25&offset=0&rating=g&lang=en`;
+
 
 class GifListContainer extends Component {
   state={
